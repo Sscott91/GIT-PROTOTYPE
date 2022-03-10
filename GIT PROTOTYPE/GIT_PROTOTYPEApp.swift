caeca899b9b68a1a865bdbf3a9a9f4326a8acac5
Stephanie Scott <scotts@union.k12.ms.us>
2022-03-10 10:23:04 -0600

    Initial Commit

---
+++ GIT PROTOTYPE/GIT_PROTOTYPEApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIT_PROTOTYPEApp.swift
+//  GIT PROTOTYPE
+//
+//  Created by Stephanie Scott on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GIT_PROTOTYPEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
