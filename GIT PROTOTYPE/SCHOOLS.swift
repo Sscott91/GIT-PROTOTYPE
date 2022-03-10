81f530b31e19f5f7d6d68d07545d87c4ad47cb98
Stephanie Scott <scotts@union.k12.ms.us>
2022-03-10 10:33:17 -0600

    TEST NEW

---
+++ GIT PROTOTYPE/SCHOOLS.swift
@@ -0,0 +1,20 @@
+//
+//  SCHOOLS.swift
+//  GIT PROTOTYPE
+//
+//  Created by Stephanie Scott on 3/10/22.
+//
+
+import SwiftUI
+
+struct SCHOOLS: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SCHOOLS_Previews: PreviewProvider {
+    static var previews: some View {
+        SCHOOLS()
+    }
+}
