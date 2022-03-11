a622d1f8ff0c819fdc29dff650703c17366dd104
Alex Rowland <rowlanda@union.k12.ms.us>
2022-03-11 13:54:22 -0600

    Test 1

--- GIT PROTOTYPE/SCHOOLS.swift
+++ GIT PROTOTYPE/SCHOOLS.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SCHOOLS: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Trying to figure this out")
     }
 }
 
