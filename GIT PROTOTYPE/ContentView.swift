f9a7c3906fdfc8b3217e06b2f1c3fbaef40ce83c
Stephanie Scott <scotts@union.k12.ms.us>
2022-03-11 10:21:07 -0600

    TEST 2

--- GIT PROTOTYPE/ContentView.swift
+++ GIT PROTOTYPE/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("GIT")
+        Text("TEST 2")
             .font(.largeTitle)
             
             .fontWeight(.bold)
