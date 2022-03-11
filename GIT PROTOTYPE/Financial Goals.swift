2cd7a85af00141869ce07ac72d5707d4747bf958
Stephanie Scott <scotts@union.k12.ms.us>
2022-03-11 09:12:02 -0600

    Here are a few changes.

---
+++ GIT PROTOTYPE/Financial Goals.swift
@@ -0,0 +1,21 @@
+//
+//  Financial Goals.swift
+//  GIT PROTOTYPE
+//
+//  Created by Stephanie Scott on 3/11/22.
+//
+
+import SwiftUI
+
+struct Financial_Goals: View {
+    var body: some View {
+        Text("Financial Goals")
+            .font(.largeTitle)
+    }
+}
+
+struct Financial_Goals_Previews: PreviewProvider {
+    static var previews: some View {
+        Financial_Goals()
+    }
+}
